dcfeff146bac19721ff4a298bfbb394719e20441
Devessier <baptiste@devessier.fr>
2022-02-27 19:43:45 +0100

    feat: download document and share it

--- App.tsx
+++ App.tsx
@@ -12,9 +12,26 @@ import {
 } from 'native-base';
 import { Feather } from '@expo/vector-icons';
 import PdfReader from 'rn-pdf-reader-js';
+import { shareAsync } from 'expo-sharing';
+import { downloadAsync, cacheDirectory } from 'expo-file-system';
 
 export default function App() {
+  if (cacheDirectory === null) {
+    throw new Error('cacheDirectory is null');
+  }
+
   const documentUri = 'https://erlang.org/download/getting_started-5.4.pdf';
+  const documentLocalUri = `${cacheDirectory}/erlang-getting-started.pdf`;
+
+  async function handleShareDocumentPress() {
+    try {
+      await downloadAsync(documentUri, documentLocalUri);
+
+      await shareAsync(documentLocalUri);
+    } catch (err) {
+      console.error('An error occured while sharing the document', err);
+    }
+  }
 
   return (
     <NativeBaseProvider>
@@ -56,6 +73,7 @@ export default function App() {
                 _pressed={{
                   bg: 'gray.100',
                 }}
+                onPress={handleShareDocumentPress}
               />
             </HStack>
           </Box>
